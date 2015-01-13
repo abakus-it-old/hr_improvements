f64a003c92238782c68ec3c9606e53fcdfda2db0
Valentin Thirion <valentinthirion@gmail.com>
2015-01-13 12:25:55 +0100

    First working version

---
+++ __openerp__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+##############################################################################
+#
+#    Copyright 2014 AbAKUS it-solutions SARL
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'version': '1.0',
+    'author': "Valentin THIRION, AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'Human resources',
+    "name": "Human resources AbAKUS improvements",
+    "description": """
+This module adds a some improvements for the HR module.
+It adds:
+- a date field for the entry of the employee in the company
+ """,
+    "depends": [
+        "hr",
+    ],
+    "data": ["hr_view.xml",],
+    'installable': True
+}
